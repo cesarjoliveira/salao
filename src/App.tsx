19966a2ff27ff0c88929564b39b8835e2fcd8dfb
cesarjoliveira <cesar.jose.oliveira@gmail.com>
2021-07-02 22:08:24 -0300

    inicio

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Pedro e Bino
+          Teste Primario do App
         </p>
         <a
           className="App-link"
